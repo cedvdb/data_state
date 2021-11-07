5e2b61fde161911f688fdf798763e0c5f2d43439
cedvdb <cedvandenbosch@gmail.com>
2021-11-07 21:20:13 +0100

    licences etc

--- example/data_state_example.dart
+++ example/data_state_example.dart
@@ -4,6 +4,7 @@ void main() {
   const loading = DataStateLoading();
   print(loading);
   final loaded = DataState.loaded(3);
+  print(loaded.data);
   print(loaded is DataStateExists);
   final notExist = DataState.loaded(null);
   final notExistList = DataState.loaded([]);
